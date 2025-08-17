11a9658fb6ab06c30eb38ca50e110cefdab568f9
Jorge Schrauwen <sjorge@acheron.be>
2025-08-17 13:51:19 +0200

    feat: Extend Jellyfin webhook payload types for media info and user events
    - Introduced ExtendMediaInfoPayload and ExtendUserEventPayload types to encapsulate common properties.
    - Updated PlaybackStopPayload to include extended media and user event properties.
    - Created UserDataSavedPayload to define properties related to user data saving events.

--- src/lib/jellyfin/webhook.ts
+++ src/lib/jellyfin/webhook.ts
@@ -1,5 +1,6 @@
 /**
- * Type describing a common properties of the Jellyfin webhook payload
+ * Type describing all common properties of the Jellyfin webhook payload
+ * @remark We can cast incomming payload data to this type, we can then use the common fields to further handle and recast the payload later.
  */
 export type BasePayload = {
   ServerId: string;
@@ -12,32 +13,70 @@ export type BasePayload = {
 };
 
 /**
- * Type partially describing the PlaybackStop properties the Jellyfin webhook payload
+ * Media Related Properties of the Jellyfin webhook payload
+ * @remark Not exported as this is not usable on it's own but both PlaybackStop and UserDataSaved have this media information
  */
-export type PlaybackStopPayload = BasePayload & {
+type ExtendMediaInfoPayload = {
   Name: string;
   Overview: string;
   Tagline: string;
-  UserId: string;
-  NotificationUsername: string;
   ItemId: string;
+  ItemType: string;
   SeriesId: string;
   SeasonId: string;
-  ItemType: string;
   RunTimeTicks: string;
   RunTime: string;
   PlaybackPositionTicks: number;
   PlaybackPosition: string;
-  IsPaused: boolean;
-  PlayedToCompletion: boolean;
   Provider_anidb?: string;
   Provider_anilist?: string;
   SeriesName: string;
   EpisodeNumber: number;
   SeasonNumber: number;
-  DeviceId: string;
-  DeviceName: string;
-  ClientName: string;
 };
+
+/**
+ * User Related Properties of the Jellyfin webhook payload
+ * @remark Not exported as this is not usable on it's own but both PlaybackStop and UserDataSaved have this user information
+ */
+type ExtendUserEventPayload = {
+  UserId: string;
+  NotificationUsername: string;
+};
+
+/**
+ * Type partially describing the PlaybackStop properties of the Jellyfin webhook payload
+ */
+export type PlaybackStopPayload = BasePayload &
+  ExtendMediaInfoPayload &
+  ExtendUserEventPayload & {
+    IsPaused: boolean;
+    PlayedToCompletion: boolean;
+    DeviceId: string;
+    DeviceName: string;
+    ClientName: string;
+  };
+
+/**
+ * Type partially describing the UserDataSaved properties of the Jellyfin webhook payload
+ */
+export type UserDataSavedPayload = BasePayload &
+  ExtendMediaInfoPayload &
+  ExtendUserEventPayload & {
+    Liked: boolean;
+    Rating: number;
+    PlayCount: number;
+    Favorite: boolean;
+    Played: boolean;
+    LastPlayedDate: string;
+    SaveReason:
+      | "PlaybackStart"
+      | "PlaybackProgress"
+      | "PlaybackFinished"
+      | "TogglePlayed"
+      | "UpdateUserRating"
+      | "Import"
+      | "UpdateUserData";
+  };
 
 // vim: tabstop=2 shiftwidth=2 softtabstop=0 smarttab expandtab
